f68d92cf3547b162cf0cd5ea2dcc0e2a9e7bdf82
Petar <pjalusic@gmail.com>
2020-02-06 14:45:31 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ocr-tesseract-wrapper",
-    version="0.0.2",
+    version="0.0.3",
     author="Petar Jalusic",
     author_email="apjalusic@gmail.com",
     description="Tiny wrapper around pytesseract with image preprocessing and OCR configurations",
